50c686ef962aadd7b7ed70a13eaa0ba49f7322cb
Vinaykumar Masane <yes2vinay@gmail.com>
2020-07-06 18:49:58 +0530

    Added Function Call Sample Code

---
+++ basics/01_getting_started/001_hello_world/hello_func.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello there, Learning Go Programming...")
+  foo()
+  fmt.Println("Back to Main, Goodbye...")
+}
+
+func foo() {
+  fmt.Println("I'm in foo")
+}
