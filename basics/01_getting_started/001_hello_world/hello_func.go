c351c5906693da71aefb5387021ef76f0f1cb79f
Vinaykumar Masane <yes2vinay@gmail.com>
2020-07-06 18:53:49 +0530

    go fmet formatted source file/s

--- basics/01_getting_started/001_hello_world/hello_func.go
+++ basics/01_getting_started/001_hello_world/hello_func.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello there, Learning Go Programming...")
-  foo()
-  fmt.Println("Back to Main, Goodbye...")
+	fmt.Println("Hello there, Learning Go Programming...")
+	foo()
+	fmt.Println("Back to Main, Goodbye...")
 }
 
 func foo() {
-  fmt.Println("I'm in foo")
+	fmt.Println("I'm in foo")
 }
